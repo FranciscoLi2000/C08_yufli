0739b9926c22486c4a3ec51d2715d84b2f5ad678
FranciscoLi2000 <francisco2000li@icloud.com>
2025-02-05 09:27:46 +0000

    print the array of struct

---
+++ ex05/ft_show_tab.c
@@ -0,0 +1,53 @@
+#include "ft_stock_str.h"
+#include <unistd.h>
+#include <limits.h>
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	str[i] = '\0';
+}
+void	ft_putnbr(int nb)
+{
+	char	c;
+
+	if (nb == INT_MIN)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	c = nb % 10 + '0';
+	write(1, &c, 1);
+}
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].str != NULL)
+	{
+		ft_putnbr(par[i].size);
+		write(1, "\n", 1);
+		ft_putstr(par[i].str);
+		write(1, "\n", 1);
+		ft_putstr(par[i].copy);
+		write(1, "\n", 1);
+		i++;
+	}
+}
